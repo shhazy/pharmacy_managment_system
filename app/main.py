304795c8812de2aa071013f380747d8d6b7d8262
Shahbaz <shahbaz@eiglou.com>
2026-01-12 19:20:48 +0500

    cors fix

--- app/main.py
+++ app/main.py
@@ -44,9 +44,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://pharmacymanagmentsystem-production.up.railway.app",
+        "https://pharmacymanagmentsystemfrontendt.vercel.app",
     ],
-    allow_origin_regex=r"http://.*\.localhost:5173",
+    # allow_origin_regex=r"http://.*\.localhost:5173",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
