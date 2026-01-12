c57d0cd26e274d0d55809d380f4d4fe220316558
Shahbaz <shahbaz@eiglou.com>
2026-01-12 18:55:07 +0500

    cors fix

--- app/main.py
+++ app/main.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://pharmacymanagmentsystem-production.up.railway.app",
     ],
     allow_origin_regex=r"http://.*\.localhost:5173",
     allow_credentials=True,
